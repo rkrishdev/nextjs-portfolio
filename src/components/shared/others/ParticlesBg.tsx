b40fdaf53e1f40928a5eba4d9da598fb91e8b2ae
Rakshankrishnan <166123920+rkrishdev@users.noreply.github.com>
2024-09-23 15:26:14 +0530

    Update ParticlesBg.tsx

--- src/components/shared/others/ParticlesBg.tsx
+++ src/components/shared/others/ParticlesBg.tsx
@@ -1,66 +1,95 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import particleStyles from "@/styles/particles-bg.module.css";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useMemo, useRef } from "react";
-import * as THREE from "three";
 
-const Particles = ({ numParticles }: { numParticles: number }) => {
-  const particlesRef = useRef<(THREE.Mesh | null)[]>([]);
+interface Particle {
+  x: number;
+  y: number;
+  size: number;
+  velX: number;
+  velY: number;
+}
 
-  const particlesPositions = useMemo(() => {
-    const positions = [];
-    for (let i = 0; i < numParticles; i++) {
-      positions.push([
-        Math.random() * 10 - 7,
-        Math.random() * 10 - 7,
-        Math.random() * 10 - 7,
-      ]);
-    }
-    return positions;
-  }, [numParticles]);
-
-  useFrame(() => {
-    particlesRef.current.forEach((particle) => {
-      if (!particle) return;
-      const vel = 0.0025;
-      particle.position.x += vel;
-      particle.position.y += vel;
-      particle.position.z += vel;
-
-      if (particle.position.z > 7) particle.position.z = -5;
-      if (particle.position.x > 7) particle.position.x = -4;
-      if (particle.position.y > 7) particle.position.y = -4;
-    });
-  });
-
-  return (
-    <>
-      {particlesPositions.map(([x, y, z], i) => (
-        <mesh
-          key={i}
-          ref={(el) => {
-            particlesRef.current[i] = el;
-          }}
-          position={[x, y, z]}
-        >
-          <sphereGeometry args={[0.0015, 2, 2]} />
-          <meshPhongMaterial color={"#ffffff"} />
-        </mesh>
-      ))}
-    </>
-  );
+const createParticle = (
+  canvasWidth: number,
+  canvasHeight: number
+): Particle => {
+  return {
+    x: Math.random() * canvasWidth,
+    y: Math.random() * canvasHeight,
+    size: Math.random() * 0.75 + 0.25,
+    velX: Math.random() * 2 + 1,
+    velY: -Math.random() * 2 - 1,
+  };
 };
 
 export const ParticlesBg = () => {
-  const numParticles = 500;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const numParticles = 150;
+
+  const resizeCanvas = () => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      canvas.width = document.body.clientWidth;
+      canvas.height = document.body.clientHeight;
+    }
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const context = canvas.getContext("2d");
+    if (!context) return;
+
+    resizeCanvas();
+    const { width, height } = canvas;
+    const particles: Particle[] = Array.from({ length: numParticles }, () =>
+      createParticle(width, height)
+    );
+
+    const render = () => {
+      context.clearRect(0, 0, width, height);
+
+      particles.forEach((particle, index) => {
+        particle.x += particle.velX;
+        particle.y += particle.velY;
+
+        if (particle.x > width) {
+          particles[index] = createParticle(width, height);
+          particles[index].x = 0;
+          particles[index].y = Math.random() * height;
+        } else if (particle.y < 0) {
+          particles[index] = createParticle(width, height);
+          particles[index].x = Math.random() * width;
+          particles[index].y = height;
+        } else if (particle.y > height) {
+          particles[index] = createParticle(width, height);
+          particles[index].x = Math.random() * width;
+          particles[index].y = 0;
+        }
+
+        context.beginPath();
+        context.arc(particle.x, particle.y, particle.size, 0, Math.PI * 2);
+        context.fillStyle = "#ffffff";
+        context.fill();
+      });
+
+      requestAnimationFrame(render);
+    };
+
+    render();
+    window.addEventListener("resize", resizeCanvas);
+
+    return () => {
+      window.removeEventListener("resize", resizeCanvas);
+    };
+  }, [numParticles]);
 
   return (
     <div className={particleStyles.canvasWrapper}>
-      <Canvas camera={{ position: [0, 0, 7], fov: 65 }}>
-        <ambientLight color={"#ffffff"} intensity={2} />
-        <Particles numParticles={numParticles} />
-      </Canvas>
+      <canvas ref={canvasRef} />
     </div>
   );
 };
