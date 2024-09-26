027614137d837b0cc7887faf88e385c1b5b9e39b
Raks <rakshankrishnan@gmail.com>
2024-09-26 22:48:07 +0530

    optimize footerGradientBg

--- src/components/shared/others/LargeGradientBg.tsx
+++ src/components/shared/others/LargeGradientBg.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import { usePreloader } from "@/context/PreloaderContext";
+import { useEffect, useState } from "react";
+
 export const LargeGradientBg = () => {
+  const { loading } = usePreloader();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!loading) {
+      setIsLoaded(true);
+    }
+  }, [loading]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return <div className="footerGradientBg"></div>;
 };
